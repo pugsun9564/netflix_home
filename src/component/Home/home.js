4c03b866521e372f9b0c16498bd87ef7262cd09c
SunWoong <lovering1771@naver.com>
2023-08-28 09:34:19 +0900

    netflix home

--- src/component/Home/home.js
+++ src/component/Home/home.js
@@ -190,22 +190,22 @@ export default function Home() {
         <div className="footer_service">
           <button className="serviceBtn">서비스 코드</button>
         </div>
-        <div class="copy-text">
-          <div class="copy-text-block">
+        <div className="copy-text">
+          <div className="copy-text-block">
             넷플릭스서비시스코리아 유한회사 통신판매업신고번호:
             제2018-서울종로-0426호 전화번호: 080-001-9587
           </div>
-          <div class="copy-text-block">대표: 레지널드 숀 톰프슨</div>
-          <div class="copy-text-block">이메일 주소: korea@netflix.com</div>
-          <div class="copy-text-block">
+          <div className="copy-text-block">대표: 레지널드 숀 톰프슨</div>
+          <div className="copy-text-block">이메일 주소: korea@netflix.com</div>
+          <div className="copy-text-block">
             주소: 대한민국 서울특별시 종로구 우정국로 26, 센트로폴리스 A동 20층
             우편번호 03161
           </div>
-          <div class="copy-text-block">사업자등록번호: 165-87-00119</div>
-          <div class="copy-text-block">
+          <div className="copy-text-block">사업자등록번호: 165-87-00119</div>
+          <div className="copy-text-block">
             클라우드 호스팅: Amazon Web Services Inc.
           </div>
-          <div class="copy-text-block">
+          <div className="copy-text-block">
             <a href="http://www.ftc.go.kr/bizCommPop.do?wrkr_no=1658700119">
               공정거래위원회 웹사이트
             </a>
